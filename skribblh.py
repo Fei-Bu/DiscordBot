52e3125c017712a2cc7f1870ed883d7c8ae1cf42
Yuki Murakami <y.murakami@tudelft.nl>
2020-05-06 19:07:38 +0200

    skribblh takes two arguments, with k1 and k2. Selects the first 100 common words

--- skribblh.py
+++ skribblh.py
@@ -5,18 +5,36 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-k', '--keyword', default='hello', metavar='',
-        help='keyword as a string', type= str)
+    parser.add_argument('-k1', '--keyword1', default='hello', metavar='',
+        help='keyword1 as a string', type= str)
+    parser.add_argument('-k2', '--keyword2', default='hello', metavar='',
+        help='keyword2 as a string', type= str)
     args = parser.parse_args()
 
-keyword = args.keyword.replace(" ", "_")
-source = requests.get('https://en.wikipedia.org/wiki/' + keyword).text
-soup = BeautifulSoup(source, 'html.parser')
+keyword1 = args.keyword1.replace(" ", "_")
+source1 = requests.get('https://en.wikipedia.org/wiki/' + keyword1).text
+soup1 = BeautifulSoup(source1, 'html.parser')
+
+word_list1 = []
+for link in soup1("a"):
+    text = link.get('title')
+    if text != None and text.replace(' ', '').isalpha() and not text.startswith('List') and not text.startswith('Outlines'):
+        word_list1.append(text)
+
+keyword2 = args.keyword2.replace(" ", "_")
+source2 = requests.get('https://en.wikipedia.org/wiki/' + keyword2).text
+soup2 = BeautifulSoup(source1, 'html.parser')
+
+word_list2 = []
+for link in soup2("a"):
+    text = link.get('title')
+    if text != None and text.replace(' ', '').isalpha() and not text.startswith('List') and not text.startswith('Outlines'):
+        word_list2.append(text)
 
 word_list = []
-for link in soup("a"):
-    text = link.get('title')
-    if text != None and text.replace(' ', '').isalpha():
-        word_list.append(text)
 
-print(word_list)
+for item in word_list1:
+    if item in word_list2:
+        word_list.append(item)
+
+print(word_list[:100])
